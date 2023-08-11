e3a8704b4a9867a311901a876835894fe39e4d87
전원재(Wonjai Jeon) <wj.jeon@konai.com>
2023-08-11 09:58:32 +0900

    Class1: Init Project

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -32,5 +32,7 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: "+(findMember == member));
     }
 }
